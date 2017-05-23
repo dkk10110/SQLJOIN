f2c29529b2241dd449e782450f87baa91c9deea0
DILEEP <dileep.kesharwani4@gmail.com>
2017-05-23 11:57:31 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,1 +1,8 @@
 
+select CUSTOMERS.NAME, CUSTOMERS.AGE,CUSTOMERS.SALARY,ord.amount, ord.oid, ord.customer_id as customerid,shp.shopamount as dileep,shp.name 
+from ORDERS as ord  
+LEFT JOIN CUSTOMERS 
+ON  ord.customer_id = CUSTOMERS.ID
+LEFT JOIN SHOP as shp
+ON shp.oid_id = ord.oid 
+GROUP BY ord.customer_id
